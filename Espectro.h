9ed2adc80f5cbbe4b00a888fe17d83af27012b74
Sebastian Mansilla <119966292+M4nsi00@users.noreply.github.com>
2024-11-24 16:57:30 -0600

    Add files via upload

---
+++ Espectro.h
@@ -0,0 +1,57 @@
+#ifndef ESPECTRO_H
+#define ESPECTRO_H
+
+#include "Enemigo.h"
+#include <iostream>
+
+using namespace std;
+
+class Espectro : public Enemigo{
+	
+	private: 
+	int tiempo_invisible; //Variable que determina cuanto tiempo el enemigo dura invicible
+	
+	public:
+		//Constructores
+		Espectro(){
+			tiempo_invisible = 3;
+		}
+		
+		Espectro(int _tiempo_invisible, float _vida, float _velocidad, float _posicion_x, float _posicion_y) : Enemigo(_vida, _velocidad, _posicion_x, _posicion_y){
+			tiempo_invisible = _tiempo_invisible;
+		}
+		
+		//Getters
+		int get_tiempo_invisible(){
+			return tiempo_invisible;
+		}
+		
+		//Setters
+		void set_tiempo_invisible(int _tiempo_invisible){
+			tiempo_invisible = _tiempo_invisible;
+		}
+		
+		//Metodos
+		void invisible(int tiempo){
+			tiempo_invisible = tiempo;
+			cout << "Duración de la invisibilidad: " << tiempo_invisible << " segundos" << endl;
+		}
+		
+		void mover() override {  // Sobreescribir el método de movimiento
+        if (tiempo_invisible > 0) {
+            cout << "Espectro invisible, no se mueve." << std::endl;
+            tiempo_invisible--;
+        } else {
+            Enemigo::mover();
+        }
+    }
+		
+		void imprimeDatos(){
+			cout << "---- Enemigo espectro ----" << endl;
+			Enemigo::imprimeDatos();
+			cout << "Duración de la invisibilidad: " << tiempo_invisible << " segundos" << endl;
+		}
+		
+};
+
+#endif
